d2ad234ccdd5fb8de4998f2de43d55f4defd68ee
Your Name <you@example.com>
2023-01-27 15:07:14 +0200

    menu fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,11 @@
 	import { page } from '$app/stores'
 
 	import '../styles/app.css'
-
+    
+	let setCheck = false;
+	function toggle() {
+		setCheck = !setCheck;
+	}
 </script>
 
 <svelte:head>
@@ -17,27 +21,27 @@
         <!-- Logo -->
         <a href="#" class="logo">Class Register</a>
         <!-- Hamburger icon -->
-        <input class="side-menu" type="checkbox" id="side-menu"/>
+        <input class="side-menu" type="checkbox" id="side-menu" bind:checked={setCheck} />
         <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
         <!-- Menu -->
         <nav class="nav">
             <ul class="menu">
 				{#if !$page.data.user}
-					<li><a href="/login">Login</a></li>
-					<li><a href="/attendance">Attendance</a></li>
+					<li><a href="/login" on:click={toggle}>Login</a></li>
+					<li><a href="/attendance" on:click={toggle}>Attendance</a></li>
 			
 					<!-- <a href="/register">Register</a> -->
 				{/if}
 			
 				{#if $page.data.user}
-					<li><a href="/admin">Admin</a></li>
-					<li><a href="/profile">Profile</a></li>
-					<li><a href="/attendance">Attendance</a></li>
+					<li><a href="/admin" on:click={toggle}4444444444444>Admin</a></li>
+					<li><a href="/profile" on:click={toggle}>Profile</a></li>
+					<li><a href="/attendance" on:click={toggle}>Attendance</a></li>
 			
-					<li><a href="/view_attendance">View Attendance</a></li>
-					<li><a href="/class_times">Class Times</a></li>
-					<li><a href="/add_user">Add User</a></li>
-					<li><a href="/list_users">List Users</a></li>
+					<li><a href="/view_attendance" on:click={toggle}>View Attendance</a></li>
+					<li><a href="/class_times" on:click={toggle}>Class Times</a></li>
+					<li><a href="/add_user" on:click={toggle}>Add User</a></li>
+					<li><a href="/list_users" on:click={toggle}>List Users</a></li>
 					<li>
 						<form
 							class="logout"
@@ -50,7 +54,8 @@
 								}
 							}}
 							>
-							<button type="submit" style="padding:20px;">Log out</button>
+							<button type="submit" style="padding:20px;" on:click={toggle}
+							>Log out</button>
 						</form>
 					</li>
 				{/if}
