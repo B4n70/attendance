3df6d7047d0e0e1174ee68e55e9663388266ede5
Your Name <you@example.com>
2023-02-01 12:03:19 +0200

    addmig email functionality

--- src/routes/(protected)/list_users/+page.server.ts
+++ src/routes/(protected)/list_users/+page.server.ts
@@ -28,54 +28,69 @@ export const load: PageServerLoad = async ({ locals, fetch }: LoadEvent) => {
 /*
 const add_time: Action = async ({ request }) => {
 */
-function myFunction(item) {
-	console.log(item)
-	console.log('sending email')
-  }
 
 export const actions = {
 	send_user_email: async ({ request, fetch }) => {
+
+		async function fetchUsers(item) {
+			let tUser = await fetch(`/api/getUser/${item}`, { headers: { 'Content-Type': 'application/json' } }).then(x => x.json())
+			//console.log(tUser)
+			return tUser
+		}
+
+		function sendMail(bUser) {
+
+			console.log(bUser.fname)
+			
+			let emailhtml = ''
+			emailhtml = "<table>"
+			emailhtml += "	<tr><th>Name</th><th>Surname</th><th>Year</th><th>Student Number</th><th></th></tr>"
+			emailhtml += "	<tr><td>"+bUser.fname+"</td>"
+			emailhtml += "		<td>"+bUser.surname+"</td>"
+			emailhtml += "		<td>"+bUser.student_year+"</td>"
+			emailhtml += "		<td>"+bUser.student_number+"</td>"
+			emailhtml += "</table>"
+		
+			let emailStatus = ''
+			//return emailStatus
+			//let emailStatus
+			sgMail.setApiKey(env.SENDGRID_API_KEY)
+			const msg = {
+			to: 'stevenjoubert@gmail.com', // Change to your recipient
+			from: 'stevenjoubert@gmail.com', // Change to your verified sender
+			subject: 'Sending with SendGrid is Fun',
+			
+			text: 'anworking!!!!!!! js',
+			//html: '<strong>anwith HTMLjs</strong>',
+			html: emailhtml,
+		
+			}
+			sgMail
+			.send(msg)
+			.then(() => {
+				emailStatus = 'Email sent'
+				return emailStatus
+			})
+			.catch((error) => {
+				console.error(error)
+			})
+		}
+
+
 		const formdata = await request.formData();
-		const userStudentNo = formdata.getAll('usersStudent');
-		userStudentNo.forEach(myFunction)
+		const userStudentNo = Array.from(formdata.getAll('usersStudent'));
+		//const result = await Promise.allSettled(userStudentNo.map(fetchUsers)).then(sendMail);
+
+		const result = await Promise.allSettled(userStudentNo.map(x => fetchUsers(x).then(sendMail)))
+
+
+		//console.log(userStudentNo)
+		//console.log(result)
 
 		return userStudentNo
 		
-		let useToEmail = await fetch(`/api/getUser/${userStudentNo}`, { headers: { 'Content-Type': 'application/json' } }).then(x => x.json())
+		//let useToEmail = await fetch(`/api/getUser/${userStudentNo}`, { headers: { 'Content-Type': 'application/json' } }).then(x => x.json())
         
-		let emailhtml = ''
-		emailhtml = "<table>"
-		emailhtml += "	<tr><th>Name</th><th>Surname</th><th>Year</th><th>Student Number</th><th></th></tr>"
-		emailhtml += "	<tr><td>"+useToEmail.fname+"</td>"
-		emailhtml += "		<td>"+useToEmail.surname+"</td>"
-		emailhtml += "		<td>"+useToEmail.student_year+"</td>"
-		emailhtml += "		<td>"+useToEmail.student_number+"</td>"
-		emailhtml += "</table>"
-		//console.log(res)
-
-
-		let emailStatus = ''
-		//return emailStatus
-		//let emailStatus
-		sgMail.setApiKey(env.SENDGRID_API_KEY)
-		const msg = {
-		to: 'stevenjoubert@gmail.com', // Change to your recipient
-		from: 'stevenjoubert@gmail.com', // Change to your verified sender
-		subject: 'Sending with SendGrid is Fun',
 		
-		text: 'anworking!!!!!!! js',
-		//html: '<strong>anwith HTMLjs</strong>',
-		html: emailhtml,
-
-		}
-		sgMail
-		.send(msg)
-		.then(() => {
-			emailStatus = 'Email sent'
-			return emailStatus
-		})
-		.catch((error) => {
-			console.error(error)
-		})
 	}
 }
