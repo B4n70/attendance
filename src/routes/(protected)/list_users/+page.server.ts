9942ee845185844879638bde81b9e9266573f72c
Your Name <you@example.com>
2023-02-17 16:13:05 +0200

    email add change

--- src/routes/(protected)/list_users/+page.server.ts
+++ src/routes/(protected)/list_users/+page.server.ts
@@ -51,7 +51,7 @@ export const actions = {
 			sgMail.setApiKey(env.SENDGRID_API_KEY)
 			const msg = {
 			to: bUser.user_email, // Change to your recipient
-			from: 'stevenjoubert@gmail.com', // Change to your verified sender
+			from: 'cfcibc.northwest@gmail.com', // Change to your verified sender
 			subject: 'Student card for '+bUser.fname,
 			text: 'its working js',
 			html: emailhtml,
